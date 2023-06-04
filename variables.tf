08481f7a8b5dff0ec01c648ef4786661da5cf9b7
gnomeguyaz@outlook.com <gnomeguyaz@outlook.com>
2023-06-04 20:53:50 +1000

    modify provider in main tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,13 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
+variable "AWS_ACCESS_KEY_ID" {
+  description = "My AWS access key"
+}
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "My AWS secret key"
+}
+
 variable "aws_region" {
   description = "AWS region for all resources."
   type        = string
